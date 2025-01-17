af258cb12c14d3b50ba1c1cc8778061c0cec55d1
Ranindu Geekiyanage <ranindu.rag@gmail.com>
2025-01-17 09:15:21 +0530

    initial commit after framework done

---
+++ app/(site)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    
+    <section className="py-40">
+      <div className="container">
+        <h1 className="text-3xl font-bold">Home Page</h1>
+      </div>
+    </section>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Playfair } from "next/font/google";
 import "./globals.css";
+import Providers from "@/components/providers";
+import { cn } from "@/lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          'flex min-h-screen flex-col',
+          geistSans.variable,
+          geistMono.variable,
+        )}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
